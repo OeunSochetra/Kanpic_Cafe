d847811c65a8d09280610780aff0f78b2b371ea4
OeunSochetra <oeunsochetra@gmail.com>
2023-09-25 09:51:14 +0700

    push to new branch

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,23 +6,22 @@ import Link from "next/link";
 function Header() {
   return (
     <div className="flex justify-between items-center gap-2">
-
       {/* Navbar link for page */}
 
       <div className="pt-6 hidden md:block ">
         <nav className="">
           <ul className="flex gap-5 font-[600] text-[#a86666]  text-xl cursor-pointer">
-            <Link href='/menu' className="hover:text-white">
+            <Link href="/menu" className="hover:text-white">
               Menu
             </Link>
-            <Link href='/stocks' className="hover:text-white" >
+            <Link href="/stocks" className="hover:text-white">
               Stocks
             </Link>
-            <Link href='/report' className="hover:text-white">
-             Report
+            <Link href="/report" className="hover:text-white">
+              Report
             </Link>
-            <Link href='/empolyee' className="hover:text-white">
-             Empolyee
+            <Link href="/empolyee" className="hover:text-white">
+              Empolyee
             </Link>
           </ul>
         </nav>
@@ -31,16 +30,15 @@ function Header() {
       {/* Navbar link for page */}
 
       <div className="flex justify-center items-center">
-      <div>
-        <Link href="/">
-          <img src="/logo.png.png" alt="logo" className="max-w-[150px]" />
-        </Link>
-      </div>
-      <div className="flex mt-2 gap-4 justify-between items-center">
-        {/* Auth icon */}
-        <ModeToggle />
-        <UserButton />
-      </div>
+        <div>
+          <Link href="/">
+            <img src="/logo.png.png" alt="logo" className="max-w-[150px]" />
+          </Link>
+        </div>
+        <div className="flex mt-2 gap-4 justify-between items-center">
+          <ModeToggle />
+          <UserButton />
+        </div>
       </div>
     </div>
   );
